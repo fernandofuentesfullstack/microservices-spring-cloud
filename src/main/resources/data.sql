6464d7e46ef7d945abe62b964520dd20cf2831e1
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2020-11-26 20:03:16 +0100

    feat: added new product to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ INSERT INTO categories (id, name) VALUES (2, 'books');
 INSERT INTO categories (id, name) VALUES (3, 'electronics');
 
 INSERT INTO products (id, name, description, stock, price, status, create_at, category_id) VALUES (1, 'adidas', 'Running shoe from ADIDAS', 5., 89.95, 'CREATED', NOW(), 1);
+INSERT INTO products (id, name, description, stock, price, status, create_at, category_id) VALUES (2, 'under armour', 'Running shoe from Under Armour', 3., 44.99, 'CREATED', NOW(), 1);
