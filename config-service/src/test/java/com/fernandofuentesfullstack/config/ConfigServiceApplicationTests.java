3e85330770b18ea7b358e641182b8bfd671ee913
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2020-11-27 19:03:16 +0100

    feat: added config-server

---
+++ config-service/src/test/java/com/fernandofuentesfullstack/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandofuentesfullstack.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
