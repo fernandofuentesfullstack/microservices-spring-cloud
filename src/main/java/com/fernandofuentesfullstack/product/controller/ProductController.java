3820cde98b33e1b8698d050ff534a237c816c683
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2020-11-27 18:13:41 +0100

    feat: added validations

--- src/main/java/com/fernandofuentesfullstack/product/controller/ProductController.java
+++ src/main/java/com/fernandofuentesfullstack/product/controller/ProductController.java
@@ -1,15 +1,24 @@
 package com.fernandofuentesfullstack.product.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fernandofuentesfullstack.product.entity.Category;
 import com.fernandofuentesfullstack.product.entity.Product;
 import com.fernandofuentesfullstack.product.service.ProductService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@Log4j2
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
@@ -22,7 +31,7 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<Product>> listProduct (@RequestParam(name = "categoryId", required = false) Long categoryId ) {
-        List<Product> products = new ArrayList<>();
+        List<Product> products;
         if (null == categoryId) {
             products = productService.listAllProduct();
             if (products.isEmpty()) {
@@ -47,9 +56,12 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct (@RequestBody Product product) {
+    public ResponseEntity<Product> createProduct (@Valid @RequestBody Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
         Product productCreate = productService.createProduct(product);
-        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
     }
 
     @PutMapping(value = "/{productId}")
@@ -80,6 +92,28 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(product);
+    }
+
+    private String formatMessage (BindingResult result) {
+        List<Map<String,String>> errors = result.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String,String> error = new HashMap<>();
+                    error.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return error;
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        log.info(jsonString);
+        return jsonString;
+
     }
 
 }
--- src/main/java/com/fernandofuentesfullstack/product/entity/Product.java
+++ src/main/java/com/fernandofuentesfullstack/product/entity/Product.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -18,8 +21,11 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "The name must not be empty")
     private String name;
     private String description;
+
+    @Positive(message = "The stock must be greater than zero")
     private Double stock;
     private Double price;
     private String status;
@@ -31,5 +37,6 @@ public class Product {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    @NotNull(message = "The category must not be empty")
     private Category category;
 }
